65a7b6f20b8d815b5c584ccaa669d8c93b275ce7
Michael Lustig <halfjew22@gmail.com>
2020-04-26 17:59:43 -0400

    Initial

---
+++ flask-server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
---
+++ nuxt-app/pages/hello.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <p>HOST --- {{ host }}!</p>
+    <!-- <p>Where am I running? {{ name }}!</p> -->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import os from "os";
+export default {
+  async asyncData() {
+    const response = await axios({
+      method: "get",
+      url: "https://lustig-cloud-run.web.app/"
+    });
+
+    return {
+      host: os.hostname()
+    };
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  font-size: 20px;
+  text-align: center;
+  padding-top: 100px;
+}
+</style>
